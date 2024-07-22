e8a94e681bd72402d862fb52c13971b61f0a6af3
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-07-22 15:00:25 +0200

    enhancement #43: change frame id to optical center for camera

--- src/gstreamer/ros_publisher.py
+++ src/gstreamer/ros_publisher.py
@@ -20,13 +20,13 @@ class ROSPublisher(Node):  # type: ignore[misc]
 
         self._compr_img = CompressedImage()
         self._compr_img.format = "jpeg"
-        self._compr_img.header.frame_id = f"{side}_camera"
+        self._compr_img.header.frame_id = f"{side}_camera_optical"
 
         self._camera_info_publisher = self.create_publisher(CameraInfo, f"teleop_camera/{self._side}_image/camera_info", 5)
         self._logger.info(f'Launching "{self._camera_info_publisher.topic_name}" publisher.')
 
         self._camera_info = CameraInfo()
-        self._camera_info.header.frame_id = f"{side}_camera"
+        self._camera_info.header.frame_id = self._compr_img.header.frame_id
         height, width, distortion_model, D, K, R, P = cam_config.to_ROS_msg(side)
         self._camera_info.height = height
         self._camera_info.width = width
